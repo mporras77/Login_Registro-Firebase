7f3decd960baae31e6dfb4db15027cb8245fd6ef
mporras77 <mporras77@misena.edu.co>
2022-11-09 09:52:47 -0500

    IngresoDeLoginRegistro

--- login-firebase/src/app/app-routing.module.ts
+++ login-firebase/src/app/app-routing.module.ts
@@ -1,27 +1,19 @@
-<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContactoComponent } from './components/contacto/contacto.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { FichaComponent } from './components/ficha/ficha.component';
 import { LoginComponent } from './components/login/login.component';
-import { PerfilComponent } from './components/perfil/perfil.component';
 import { RecuperarPasswordComponent } from './components/recuperar-password/recuperar-password.component';
 import { RegistrarUsuarioComponent } from './components/registrar-usuario/registrar-usuario.component';
 import { VerificarCorreoComponent } from './components/verificar-correo/verificar-correo.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
-
-  {path: 'login', component: LoginComponent},
-  {path: 'registrar-usuario', component: RegistrarUsuarioComponent},
-  {path: 'verificar-correo', component: VerificarCorreoComponent},
-  {path: 'recuperar-password', component: RecuperarPasswordComponent},
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'perfil', component: PerfilComponent},
-  {path: 'ficha', component: FichaComponent},
-  {path: 'contacto', component: ContactoComponent},
-  {path: '**', redirectTo: 'login', pathMatch: 'full'},
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'registrar-usuario', component: RegistrarUsuarioComponent },
+  { path: 'verificar-correo', component: VerificarCorreoComponent },
+  { path: 'recuperar-password', component: RecuperarPasswordComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', redirectTo: 'login', pathMatch: 'full' },
 ];
 
 @NgModule({
@@ -29,35 +21,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-=======
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { ContactoComponent } from './components/contacto/contacto.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { FichaComponent } from './components/ficha/ficha.component';
-import { LoginComponent } from './components/login/login.component';
-import { PerfilComponent } from './components/perfil/perfil.component';
-import { RecuperarPasswordComponent } from './components/recuperar-password/recuperar-password.component';
-import { RegistrarUsuarioComponent } from './components/registrar-usuario/registrar-usuario.component';
-import { VerificarCorreoComponent } from './components/verificar-correo/verificar-correo.component';
-
-const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
-
-  {path: 'login', component: LoginComponent},
-  {path: 'registrar-usuario', component: RegistrarUsuarioComponent},
-  {path: 'verificar-correo', component: VerificarCorreoComponent},
-  {path: 'recuperar-password', component: RecuperarPasswordComponent},
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'perfil', component: PerfilComponent},
-  {path: 'ficha', component: FichaComponent},
-  {path: 'contacto', component: ContactoComponent},
-  {path: '**', redirectTo: 'login', pathMatch: 'full'},
-];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
->>>>>>> 15c4671485021979fa4a49b563da489a7e9523a2
--- login-firebase/src/app/app.module.ts
+++ login-firebase/src/app/app.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Modulos
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+// Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
@@ -9,16 +16,8 @@ import { RegistrarUsuarioComponent } from './components/registrar-usuario/regist
 import { VerificarCorreoComponent } from './components/verificar-correo/verificar-correo.component';
 import { RecuperarPasswordComponent } from './components/recuperar-password/recuperar-password.component';
 import { SpinnerComponent } from './shared/spinner/spinner.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import {AngularFireModule} from '@angular/fire/compat';
+
 import { environment } from 'src/environments/environment';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToastrModule } from 'ngx-toastr';
-import { VistainicioComponent } from './components/vistainicio/vistainicio.component';
-import { PerfilComponent } from './components/perfil/perfil.component';
-import { ContactoComponent } from './components/contacto/contacto.component';
-<<<<<<< HEAD
-import { FichaComponent } from './components/ficha/ficha.component';
 
 @NgModule({
   declarations: [
@@ -28,51 +27,17 @@ import { FichaComponent } from './components/ficha/ficha.component';
     RegistrarUsuarioComponent,
     VerificarCorreoComponent,
     RecuperarPasswordComponent,
-    SpinnerComponent,
-    VistainicioComponent,
-    PerfilComponent,
-    ContactoComponent,
-    FichaComponent
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    BrowserAnimationsModule, 
-    ToastrModule.forRoot(), 
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-=======
-import { FichaComponent } from './components/ficha/ficha.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    DashboardComponent,
-    RegistrarUsuarioComponent,
-    VerificarCorreoComponent,
-    RecuperarPasswordComponent,
-    SpinnerComponent,
-    VistainicioComponent,
-    PerfilComponent,
-    ContactoComponent,
-    FichaComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    BrowserAnimationsModule, 
-    ToastrModule.forRoot(), 
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
->>>>>>> 15c4671485021979fa4a49b563da489a7e9523a2
