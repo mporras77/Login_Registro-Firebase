9081664f88586c0e5c0e9e5c0e1e25f5b7e6f698
mporras77 <mporras77@misena.edu.co>
2022-11-10 08:34:03 -0500

    IngresoDeLoginRegistro

--- login-firebase/src/app/app-routing.module.ts
+++ login-firebase/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './components/contacto/contacto.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FichaComponent } from './components/ficha/ficha.component';
 import { LoginComponent } from './components/login/login.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
 import { RecuperarPasswordComponent } from './components/recuperar-password/recuperar-password.component';
 import { RegistrarUsuarioComponent } from './components/registrar-usuario/registrar-usuario.component';
 import { VerificarCorreoComponent } from './components/verificar-correo/verificar-correo.component';
+import { VistainicioComponent } from './components/vistainicio/vistainicio.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -13,6 +17,10 @@ const routes: Routes = [
   { path: 'verificar-correo', component: VerificarCorreoComponent },
   { path: 'recuperar-password', component: RecuperarPasswordComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'ficha', component: FichaComponent},
+  { path: 'perfil', component: PerfilComponent},
+  { path: 'contacto', component: ContactoComponent},
+  { path: 'vistainicio', component: VistainicioComponent},
   { path: '**', redirectTo: 'login', pathMatch: 'full' },
 ];
 
