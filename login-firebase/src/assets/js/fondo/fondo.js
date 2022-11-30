8db3772b689a81021150954db4151f11066616ec
mporras77 <mporras77@misena.edu.co>
2022-11-30 11:34:56 -0500

    IngresoDeLoginRegistro

--- login-firebase/src/assets/js/fondo/fondo.js
+++ login-firebase/src/assets/js/fondo/fondo.js
@@ -1,57 +1,16 @@
-// ----- On render -----
-$(function() {
-
-    $('#profile').addClass('dragging').removeClass('dragging');
+$(document).ready(function(){
+  // Prepare the preview for profile picture
+      $("#wizard-picture").change(function(){
+          readURL(this);
+      });
   });
+  function readURL(input) {
+      if (input.files && input.files[0]) {
+          var reader = new FileReader();
   
-  $('#profile').on('dragover', function() {
-    $('#profile').addClass('dragging')
-  }).on('dragleave', function() {
-    $('#profile').removeClass('dragging')
-  }).on('drop', function(e) {
-    $('#profile').removeClass('dragging hasImage');
-  
-    if (e.originalEvent) {
-      var file = e.originalEvent.dataTransfer.files[0];
-      console.log(file);
-  
-      var reader = new FileReader();
-  
-      //attach event handlers here...
-  
-      reader.readAsDataURL(file);
-      reader.onload = function(e) {
-        console.log(reader.result);
-        $('#profile').css('background-image', 'url(' + reader.result + ')').addClass('hasImage');
-  
+          reader.onload = function (e) {
+              $('#wizardPicturePreview').attr('src', e.target.result).fadeIn('slow');
+          }
+          reader.readAsDataURL(input.files[0]);
       }
-  
-    }
-  })
-  $('#profile').on('click', function(e) {
-    console.log('clicked')
-    $('#mediaFile').click();
-  });
-  window.addEventListener("dragover", function(e) {
-    e = e || event;
-    e.preventDefault();
-  }, false);
-  window.addEventListener("drop", function(e) {
-    e = e || event;
-    e.preventDefault();
-  }, false);
-  $('#mediaFile').change(function(e) {
-  
-    var input = e.target;
-    if (input.files && input.files[0]) {
-      var file = input.files[0];
-  
-      var reader = new FileReader();
-  
-      reader.readAsDataURL(file);
-      reader.onload = function(e) {
-        console.log(reader.result);
-        $('#profile').css('background-image', 'url(' + reader.result + ')').addClass('hasImage');
-      }
-    }
-  })
+  }
